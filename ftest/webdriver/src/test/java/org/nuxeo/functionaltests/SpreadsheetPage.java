a150f4e3290a6a9a306d5123030452c2dabb3725
Nelson Silva <nelson.silva@inevo.pt>
2015-04-08 12:53:27 +0100

    NXP-16911: add test for the 'save' action

--- ftest/webdriver/src/test/java/org/nuxeo/functionaltests/SpreadsheetPage.java
+++ ftest/webdriver/src/test/java/org/nuxeo/functionaltests/SpreadsheetPage.java
@@ -16,7 +16,10 @@
  */
 package org.nuxeo.functionaltests;
 
+import org.apache.commons.lang.StringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -34,16 +37,32 @@ public class SpreadsheetPage {
 
     private static final String ROWS_XPATH = "tbody/tr/*[1]";
 
+    private final WebDriver driver;
+
     @Required
     @FindBy(xpath = "//table[@class=\"htCore\"]")
     WebElement table;
 
     @Required
     @FindBy(id = "query")
-    WebElement query;
+    WebElement queryInput;
 
     @FindBy(id = "execute")
-    WebElement execute;
+    WebElement executeButton;
+
+    @FindBy(id = "save")
+    WebElement saveButton;
+
+    @FindBy(id = "close")
+    WebElement closeButton;
+
+    @Required
+    @FindBy(id = "console")
+    WebElement console;
+
+    public SpreadsheetPage(WebDriver driver) {
+        this.driver = driver;
+    }
 
     public List<WebElement> getHeaderElements() {
         return table.findElements(By.xpath(HEADERS_XPATH));
@@ -59,5 +78,62 @@ public class SpreadsheetPage {
 
     public List<WebElement> getRows() {
         return table.findElements(By.xpath(ROWS_XPATH));
+    }
+
+    public String getMessages() {
+        return console.getText();
+    }
+
+    /**
+     * Set data at given row and cell
+     */
+    public void setData(int row, int cell, Object data) {
+        callHandsontable("setDataAtCell", row, cell, data);
+    }
+
+    /**
+     * Get data at given row and cell
+     */
+    public Object getData(int row, int cell) {
+        return callHandsontable("getDataAtCell", row, cell);
+    }
+
+    /**
+     * Run a query
+     */
+    public void executeQuery(String query) {
+        queryInput.sendKeys("SELECT * FROM Document");
+        executeButton.click();
+        waitForRequests();
+    }
+
+    /**
+     * Save the data
+     */
+    public void save() {
+        saveButton.click();
+        waitForRequests();
+    }
+
+    /**
+     * Close the spreadsheet
+     */
+    public void close() {
+        closeButton.click();
+    }
+
+    /**
+     * Wait for ajax requests to complete
+     */
+    protected void waitForRequests() {
+        new AjaxRequestManager(driver).waitForJQueryRequests();
+    }
+
+    /**
+     * Call a Handsontable method
+     */
+    protected Object callHandsontable(String method, Object... params) {
+        return ((JavascriptExecutor) driver).executeScript(
+            "return $('#grid').handsontable('" + method + "', " + StringUtils.join(params, ",") + ");");
     }
 }
